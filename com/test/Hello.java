124fc365c35bf9be6a0b1a22aa4fca4b93d23a3a
SeungJuNam <skatmdwn23@naver.com>
2023-09-18 21:16:16 +0900

    Hello World Hello World문 프린트

---
+++ com/test/Hello.java
@@ -0,0 +1,16 @@
+package firstProject.com.test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		
+		//한줄 주석
+		/* 여러 줄 주석 */
+		short b = 1, c = 2;
+		//short a = b+c;
+		//System.out.println(a);
+
+	}
+
+}
